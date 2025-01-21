85c942f9a9bc226d0625c97a70f5ed2d453ec949
George Gonçalves <georgeggmiranda@gmail.com>
2025-01-20 22:47:47 -0300

    properties, Game, ORM

---
+++ src/main/java/com/example/dslist/entidades/Game.java
@@ -0,0 +1,133 @@
+package com.example.dslist.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Game implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private Integer ano;
+	private String genero;
+	private String plataforma;
+	private Double pontuacao;
+	private String imgUrl;
+	private String curtaDescricao;
+	private String longaDescricao;
+	
+	public Game() {
+	}
+
+	public Game(Long id, String titulo, Integer ano, String genero,
+			String plataforma, Double pontuacao, String imgUrl,
+			String curtaDescricao, String longaDescricao) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.ano = ano;
+		this.genero = genero;
+		this.plataforma = plataforma;
+		this.pontuacao = pontuacao;
+		this.imgUrl = imgUrl;
+		this.curtaDescricao = curtaDescricao;
+		this.longaDescricao = longaDescricao;
+	}	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Double getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(Double pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getCurtaDescricao() {
+		return curtaDescricao;
+	}
+
+	public void setCurtaDescricao(String curtaDescricao) {
+		this.curtaDescricao = curtaDescricao;
+	}
+
+	public String getLongaDescricao() {
+		return longaDescricao;
+	}
+
+	public void setLongaDescricao(String longaDescricao) {
+		this.longaDescricao = longaDescricao;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+}
